a957c0a940e2a49f97bedca1a8a8c11049b7fe5f
AmitKuveskar <Kuveskar.amit@gmail.com>
2022-06-16 11:29:04 +0530

    First

---
+++ Java Practice/src/Outer.java
@@ -0,0 +1,16 @@
+
+public class Outer {
+	static int x=10;
+		static class Inner{
+			public void demo() {
+				System.out.println("Inner Class");
+				x=100;
+			}
+		}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Outer.Inner obj = new Outer.Inner();
+				obj.demo();
+	}
+
+}
