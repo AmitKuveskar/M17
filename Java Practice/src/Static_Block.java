e470d2cfc371bbd24dbad9e41ef92bf3e15e7752
AmitKuveskar <Kuveskar.amit@gmail.com>
2022-06-16 10:21:44 +0530

    first

---
+++ Java Practice/src/Static_Block.java
@@ -0,0 +1,27 @@
+
+public class Static_Block {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+			System.out.println("Executing Main Block");
+			m();
+	}
+	static {
+		System.out.println("Vande MAtarm");
+	}
+	static {
+		System.out.println("I love India");
+	}
+	static {
+		System.out.println("Jai Hind");
+	}
+	
+	static void m() {
+	System.out.println("Bharat");
+	}
+	
+	
+	
+	
+}
