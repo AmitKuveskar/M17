4125b7e6528d18dbc2ae251a1645c84fe5593c3c
AmitKuveskar <Kuveskar.amit@gmail.com>
2022-06-21 10:27:17 +0530

    First

---
+++ Java Practice/src/Concurrency.java
@@ -0,0 +1,18 @@
+import java.util.concurrent.locks.ReentrantLock;
+
+public class Concurrency {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ReentrantLock l =new ReentrantLock();
+		l.lock();
+		System.out.print(l.isLocked());
+		System.out.print(l.isHeldByCurrentThread());
+		System.out.print(l.getQueueLength());
+		l.unlock();
+		System.out.print(l.getHoldCount());
+		System.out.print(l.isLocked());
+		
+	}
+
+}
